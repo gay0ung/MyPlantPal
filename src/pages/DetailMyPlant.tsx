04d21a3906e809101af32e5f3a4b6704ca4b5eb5
ga young <58800923+gay0ung@users.noreply.github.com>
2025-06-03 17:29:03 +0900

    feat: 내식물 상세 정보 보기, 선택한 내 식물 store에 저장하기

--- src/pages/DetailMyPlant.tsx
+++ src/pages/DetailMyPlant.tsx
@@ -1,5 +1,16 @@
+import PlantImage from '@/features/plant/PlantImage';
+import { usePlantStore } from '@/stores/plantStore';
+
 const DetailMyPlant = () => {
-    return <div></div>;
+    const plant = usePlantStore(state => state.selectedMyPlant);
+
+    return (
+        <div className="flex flex-col items-center gap-y-4">
+            <PlantImage src={plant?.imgUrl || ''} alt={`${plant?.name} 이미지`} />
+            <p>{plant?.name}</p>
+            <p>{plant?.nameEn}</p>
+        </div>
+    );
 };
 
 export default DetailMyPlant;
--- src/stores/plantStore.ts
+++ src/stores/plantStore.ts
@@ -1,17 +1,22 @@
-import { NongsaroPlant, NongsaroPlantSummary } from '@/types/nongsaroPlant';
+import { NongsaroPlantSummary } from '@/types/nongsaroPlant';
+import { Plant } from '@/types/plant';
 import { create } from 'zustand';
 
 interface Store {
+    selectedMyPlant: Plant | null;
     nongsaroPlantSummaries: NongsaroPlantSummary[] | null;
     selectedNongsaroPlantSummary: NongsaroPlantSummary | null;
+    setSelectedMyPlant: (myPlant: Plant | null) => void;
     setNongsaroPlantSummaries: (gardenPlants: NongsaroPlantSummary[] | null) => void;
     setSelectedNongsaroPlantSummary: (plant: NongsaroPlantSummary | null) => void;
 }
 
 export const usePlantStore = create<Store>(set => ({
+    selectedMyPlant: null,
     nongsaroPlantSummaries: null,
     selectedNongsaroPlantSummary: null,
 
+    setSelectedMyPlant: plant => set({ selectedMyPlant: plant }),
     setNongsaroPlantSummaries: plantSummaries => set({ nongsaroPlantSummaries: plantSummaries }),
     setSelectedNongsaroPlantSummary: plantSummary => set({ selectedNongsaroPlantSummary: plantSummary })
 }));
