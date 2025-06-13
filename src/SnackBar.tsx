ca0f8bcd17f35914b66a06e7ad3600c99acd14b6
ga young <58800923+gay0ung@users.noreply.github.com>
2025-06-13 15:10:35 +0900

    style: 타입에 따른 배경 색상 적용

--- src/SnackBar.tsx
+++ src/SnackBar.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSnackbarStore } from './stores/snackbarStore';
 
 const SnackBar = () => {
-    const { isOpen, message, duration, close } = useSnackbarStore();
+    const { isOpen, message, duration, close, type } = useSnackbarStore();
 
     useEffect(() => {
         if (isOpen) {
@@ -14,9 +14,20 @@ const SnackBar = () => {
         }
     }, [isOpen, duration, close]);
 
+    const bgColor = useMemo(() => {
+        switch (type) {
+            case 'info':
+                return 'bg-blue-400';
+            case 'error':
+                return 'bg-red-400';
+            default:
+                return 'bg-blue-400';
+        }
+    }, [type]);
+
     return (
         <div
-            className={`fixed z-10 -translate-x-1/2 left-1/2 w-1/2 p-3 bg-blue-400 ${isOpen ? 'opacity-100 bottom-[calc(var(--navbar-height)+20px)]' : 'opacity-100 bottom-0'} transition-bottom duration-300 ease-out`}
+            className={`fixed z-10 -translate-x-1/2 left-1/2 w-1/2 p-3 ${isOpen ? 'opacity-100 bottom-[calc(var(--navbar-height)+20px)]' : 'opacity-100 bottom-0'} transition-bottom duration-300 ease-out ${bgColor}`}
             role="alert"
         >
             <p>{message}</p>
