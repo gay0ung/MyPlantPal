e3abcd880aa7e9ca09337e66b1c8f8fc66686f57
ga young <58800923+gay0ung@users.noreply.github.com>
2025-07-30 17:30:14 +0900

    style: 네비게이션 메뉴 디자인 수정

--- src/NavigationMenu.tsx
+++ src/NavigationMenu.tsx
@@ -4,21 +4,27 @@ interface MenuProps {
     ariaLabel: string;
     onClick: () => void;
     labelName: string;
+    icon: string;
 }
 
 const NavigationMenu = () => {
     const navigate = useNavigate();
 
     const Menu = (props: MenuProps) => (
-        <button type="button" onClick={props?.onClick} className="flex-1" aria-label={props?.ariaLabel}>
-            {props?.labelName}
+        <button
+            type="button"
+            onClick={props?.onClick}
+            className="flex flex-1 justify-center items-center text-3xl"
+            aria-label={props?.ariaLabel}
+        >
+            {props.icon}
         </button>
     );
 
     return (
         <div className="relative z-50 flex gap-2 justify-between bg-white">
-            <Menu onClick={() => navigate('/search')} ariaLabel="검색 화면으로 이동" labelName="검색" />
-            <Menu onClick={() => navigate('/home')} ariaLabel="홈 화면으로 이동" labelName="홈" />
+            <Menu onClick={() => navigate('/search')} ariaLabel="검색 화면으로 이동" labelName="검색" icon="🔎" />
+            <Menu onClick={() => navigate('/home')} ariaLabel="홈 화면으로 이동" labelName="홈" icon="🏠" />
         </div>
     );
 };
