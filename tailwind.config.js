9a6d7749c082f014fff54da6ec430bde23466d9c
ga young <58800923+gay0ung@users.noreply.github.com>
2025-07-30 17:29:22 +0900

    style: theme.colors 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                'primary-green': '#1A4D2E',
+                'secondary-green': '#4F6F52',
+                'primary-beige': '#E8DFCA',
+                'secondary-beige': '#F5EFE6',
+                'primary-dark': '#1A1A19'
+            }
+        }
     },
     plugins: []
 };
